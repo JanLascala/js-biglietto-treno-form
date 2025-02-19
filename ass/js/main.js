5f79f67fe5acf8a39132a9c9aed34f9ff3830389
JanLascala <lascala.jan@gmail.com>
2025-02-19 14:14:01 +0100

    Rec progress

--- ass/js/main.js
+++ ass/js/main.js
@@ -2,3 +2,40 @@ console.log("do i work")
 
 const AgeEl = document.getElementById('passAgerange')
 console.log(AgeEl)
+const kMEl = document.getElementById('Kmselect')
+console.log(kMEl)
+const submEl = document.getElementById('Submitdata')
+const priceEl = document.getElementById('price')
+
+const tckprice = 0.21;
+
+submEl.addEventListener('submit', function (e) {
+    e.defaultPrevented
+    console.log(AgeEl.value)
+    console.log(kMEl.value)
+    priceEl.innerText = AgeEl.value
+})
+
+
+
+
+
+
+
+/*if (userAge < 18) {
+
+    const minorDiscount = (80 * standardPrice) / 100;
+    const minordecimal = minorDiscount.toFixed(2)
+
+    document.getElementById("minorDiscount").innerHTML = (minordecimal);
+
+} else if (userAge > 65) {
+    const seniorDiscount = (60 * standardPrice) / 100
+
+    const seniordecimal = seniorDiscount.toFixed(2)
+
+    document.getElementById("seniorDiscount").innerHTML = (seniordecimal);
+
+
+}*/
+
