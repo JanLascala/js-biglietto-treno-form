50709a8d21484c18cc1547a212095cb4faa118cc
JanLascala <lascala.jan@gmail.com>
2025-02-19 15:38:16 +0100

    Caught minor error

--- ass/js/main.js
+++ ass/js/main.js
@@ -14,11 +14,34 @@ const standardPrice = tckprice * kMEl.value
 
 formEl.addEventListener('submit', function (event) {
     event.preventDefault();
+    const standardPrice = tckprice * kMEl.value
+    //check in console if everything works as should
     console.log(ageEl.value)
     console.log(kMEl.value)
-    const standardPrice = tckprice * kMEl.value
     console.log(standardPrice)
-    priceEl.innerText = standardPrice
+
+    if (ageEl.value = 1) {
+
+        const minorDiscount = (80 * standardPrice) / 100;
+        const minordecimal = minorDiscount.toFixed(2)
+        priceEl.innerText = minordecimal
+
+
+
+    } else if (ageEl.value = 3) {
+
+        const seniorDiscount = (60 * standardPrice) / 100
+        const seniordecimal = seniorDiscount.toFixed(2)
+        priceEl.innerText = seniordecimal
+
+
+
+    } else if (ageEl.value = 2) {
+        priceEl.innerText = standardPrice
+
+
+    }
+
 
 })
 
